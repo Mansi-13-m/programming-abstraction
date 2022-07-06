dfed31dabfaa09ceb2af324819a5db8f6cb244e7
Mansi-13-m <78354199+Mansi-13-m@users.noreply.github.com>
2022-07-06 10:25:15 +0530

    Print Size of Datatypes

---
+++ sizeofdatatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"size of data types:"<<endl;
+    cout<<"size of int:"<<sizeof(int)<<"bytes"<<endl;
+    cout<<"size of long:"<<sizeof(long)<<"bytes"<<endl;
+    cout<<"size of float:"<<sizeof(float)<<"bytes"<<endl;
+    cout<<"size of double:"<<sizeof(double)<<"bytes"<<endl;
+    cout<<"size of char:"<<sizeof(char)<<"bytes"<<endl;
+    cout<<"size of bool:"<<sizeof(bool)<<"bytes"<<endl;
+    cout<<"size of short:"<<sizeof(short)<<"bytes"<<endl;
+    return 0;
+}
